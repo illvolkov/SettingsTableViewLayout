86dbd5fab114b18e393f52ba4be7a72d7089cfbf
Ilya Volkov <illvolkov@bk.ru>
2022-02-28 19:00:54 +0500

    Project preparation

--- SettingsTableViewLayout/ViewController.swift
+++ SettingsTableViewLayout/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    //MARK: - Settings
+    
+    private func setupHiearchy() {
+        
+    }
+    
+    private func setupLayput() {
+        
+    }
+    
+    private func setupView() {
+        
     }
 
 
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && red <= 255, "Invalid green component")
+        assert(blue >= 0 && red <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(red: (rgb >> 16) & 0xFF,
+                  green: (rgb >> 8) & 0xFF,
+                  blue: rgb & 0xFF)
+    }
 }
 
